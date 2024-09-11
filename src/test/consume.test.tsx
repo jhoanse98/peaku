5c3abff1374df58b2ff5a85dc3d3002d5e854b5c
Sebas24 <sebastian.marin@lernit.com.mx>
2024-09-10 22:22:25 -0500

    chore: se ajusta el codigo de test por el eslint

--- src/test/consume.test.tsx
+++ src/test/consume.test.tsx
@@ -1,33 +1,32 @@
-import React from 'react'
-import { render, waitFor, screen } from '@testing-library/react'
-import '@testing-library/jest-dom'
-import Consume from '../components/Consume'
+import React from "react";
+import { render, screen } from "@testing-library/react";
+import "@testing-library/jest-dom";
+import Consume from "../components/Consume";
 
+describe("Consume Component", () => {
+  it("Should render correctly", () => {
+    render(<Consume />);
+    expect(screen.getByTestId("container")).toBeInTheDocument();
+  });
 
-describe('Consume Component', () => {
-    it('Should render correctly', () => {
-        const { getByTestId } = render(<Consume />);
-        expect(getByTestId('container')).toBeInTheDocument();
-    })
+  it("Should renders title and graphic container", () => {
+    render(<Consume />);
+    expect(screen.getByText("Consume Energy")).toBeInTheDocument();
+    expect(screen.getByTestId("chart")).toBeInTheDocument();
+  });
 
-    it('Should renders title and graphic container', () => {
-        const { getByText, getByTestId } = render(<Consume />);
-        expect(getByText('Consume Energy')).toBeInTheDocument();
-        expect(getByTestId('chart')).toBeInTheDocument();
-    });
+  it("Should renders legend items.", async () => {
+    render(<Consume />);
+    await screen.findByTestId("chart");
+    const chart = screen.getByTestId("chart");
+    expect(chart).toHaveAttribute("width", "800");
+    expect(chart).toHaveAttribute("height", "400");
+  });
 
-    it('Should renders legend items.', async () => {
-        const { getByTestId } = render(<Consume />);
-        await waitFor(() => getByTestId('chart'));
-        const chart = getByTestId('chart');
-        expect(chart).toHaveAttribute('width', '800');
-        expect(chart).toHaveAttribute('height', '400');
-    });
-
-    it('Should calls useEffect when svgRef changes.', () => {
-        const useEffectSpy = jest.spyOn(React, 'useEffect');
-        const { rerender } = render(<Consume />);
-        rerender(<Consume />);
-        expect(useEffectSpy).toHaveBeenCalledTimes(2);
-    })
-})
+  it("Should calls useEffect when svgRef changes.", () => {
+    const useEffectSpy = jest.spyOn(React, "useEffect");
+    const { rerender } = render(<Consume />);
+    rerender(<Consume />);
+    expect(useEffectSpy).toHaveBeenCalledTimes(2);
+  });
+});
--- src/test/losts.test.tsx
+++ src/test/losts.test.tsx
@@ -1,33 +1,32 @@
-import React from 'react'
-import { render, waitFor, screen } from '@testing-library/react'
-import '@testing-library/jest-dom'
-import Losts from '../components/Losts'
+import React from "react";
+import { render, screen } from "@testing-library/react";
+import "@testing-library/jest-dom";
+import Losts from "../components/Losts";
 
+describe("Losts Energy Component", () => {
+  it("Should render correctly", () => {
+    render(<Losts />);
+    expect(screen.getByTestId("container")).toBeInTheDocument();
+  });
 
-describe('Losts Energy Component', () => {
-    it('Should render correctly', () => {
-        const { getByTestId } = render(<Losts />);
-        expect(getByTestId('container')).toBeInTheDocument();
-    })
+  it("Should renders title and graphic container", () => {
+    render(<Losts />);
+    expect(screen.getByText("Losts Energy")).toBeInTheDocument();
+    expect(screen.getByTestId("chart")).toBeInTheDocument();
+  });
 
-    it('Should renders title and graphic container', () => {
-        const { getByText, getByTestId } = render(<Losts />);
-        expect(getByText('Losts Energy')).toBeInTheDocument();
-        expect(getByTestId('chart')).toBeInTheDocument();
-    });
+  it("Should renders legend items.", async () => {
+    render(<Losts />);
+    await screen.findByTestId("chart");
+    const chart = screen.getByTestId("chart");
+    expect(chart).toHaveAttribute("width", "800");
+    expect(chart).toHaveAttribute("height", "400");
+  });
 
-    it('Should renders legend items.', async () => {
-        const { getByTestId } = render(<Losts />);
-        await waitFor(() => getByTestId('chart'));
-        const chart = getByTestId('chart');
-        expect(chart).toHaveAttribute('width', '800');
-        expect(chart).toHaveAttribute('height', '400');
-    });
-
-    it('Should calls useEffect when svgRef changes.', () => {
-        const useEffectSpy = jest.spyOn(React, 'useEffect');
-        const { rerender } = render(<Losts />);
-        rerender(<Losts />);
-        expect(useEffectSpy).toHaveBeenCalledTimes(2);
-    })
-})
+  it("Should calls useEffect when svgRef changes.", () => {
+    const useEffectSpy = jest.spyOn(React, "useEffect");
+    const { rerender } = render(<Losts />);
+    rerender(<Losts />);
+    expect(useEffectSpy).toHaveBeenCalledTimes(2);
+  });
+});
